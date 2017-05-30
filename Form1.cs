8c12fcc0a13f98a103e0f94644137a93ec99da8a
Harrison Thacker <harrison.thacker69@gmail.com>
2017-05-29 21:35:46 -0400

    Version 0.1
    EmguCV C# Test with Visual Studio and Windows Forms.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+namespace EmguCVTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if(open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Image img = Image.FromFile(open.FileName);
+                pictureBox1.Image = img;
+
+            }
+        }
+    }
+}
